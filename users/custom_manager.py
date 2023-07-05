18f33a9be079bc7f9a94aabd9ebc859405e43f2c
Deyoungj <emmanuelonyibor39@gmail.com>
2023-07-05 16:04:52 +0100

    initial commit

--- users/custom_manager.py
+++ users/custom_manager.py
@@ -1,23 +1,30 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 
 class CustomUserManager(BaseUserManager):
-    def create_superuser(self, email: str = None, password=None, **extr_fields):
-        extr_fields.setdefault('is_staff', True)
-        extr_fields.setdefault('is_superuser', True)
-        extr_fields.setdefault('is_active', True)
 
-        if extr_fields.get('is_active') is not True:
+    def create_superuser(self, email: str = None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_active') is not True:
             raise ValueError("is_active must be set to true")
         
-        if extr_fields.get('is_superuser') is not True:
+        if extra_fields.get('is_superuser') is not True:
             raise ValueError("is_superuser must be set to true")
 
         if not email:
             raise ValueError("Email is required")
+
+
+        email = self.normalize_email(email)      
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
 
 
     def create_user(self, email, password, **extra_fileds):
@@ -33,5 +40,3 @@ class CustomUserManager(BaseUserManager):
         return user
 
 
-class User(AbstractUser):
-    pass
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .custom_manager import CustomUserManager
 
-# Create your models here.
+
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=100)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
