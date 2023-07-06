64989cf1681d04b09eab675eb9fcd140e53d73df
Deyoungj <emmanuelonyibor39@gmail.com>
2023-07-06 12:14:15 +0100

    setting up serializer

--- users/models.py
+++ users/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from .custom_manager import CustomUserManager
 from django.utils import timezone
 
 
 
 
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     email = models.EmailField(max_length=100, unique=True)
     username = models.CharField(max_length=100)
     date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
 
     objects = CustomUserManager()
     USERNAME_FIELD = 'email'
--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,30 @@
 from rest_framework import serializers
-
+from rest_framework.validators import ValidationError
+from .models import User
 
 
 
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    pass
 
+    email = serializers.EmailField(required=True, max_length=100)
+    username = serializers.CharField(max_length=100)
+    password = serializers.CharField(min_length =6, write_only=True)
+    
+
+    class Meta:
+        model = User
+        fields=['username','email', 'password']
+
+    def validate(self, attrs):
+
+        email_exist = User.objects.filter(email=attrs['email']).exists()
+
+        if email_exist:
+            raise ValidationError("This Email already exists")
+
+        return super().validate(attrs)
 
 
 
