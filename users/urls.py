7b4caf15b6537c91d9c249b99ac349947a291e3a
Deyoungj <emmanuelonyibor39@gmail.com>
2023-07-07 00:10:56 +0100

    handling passwords app urls

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,8 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name="register"),
+    path('auth/register/', views.RegisterView.as_view(), name="register"),
+    path('auth/login/', views.LoginView.as_view(), name="login"),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
