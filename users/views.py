dd59eb9176c6dbced3b22f49561f1451b0b74f32
Deyoungj <emmanuelonyibor39@gmail.com>
2023-07-06 13:12:10 +0100

    created register view

--- users/views.py
+++ users/views.py
@@ -1,1 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status, generics
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+
+
+
+class RegisterView(generics.GenericAPIView):
+
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        data = request.data
+
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+
+            
+            response = {
+                "message": "Registration successfull",
+                "data": serializer.data
+            }
+
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        
+        return Response(data=serializer.data, status=status.HTTP_400_BAD_REQUEST)
